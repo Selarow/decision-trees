5ef79a0cce6af9e68788c247c3ba28382979a39e
Selarow <selarowwastaken@gmail.com>
2023-06-26 22:23:56 +0200

    tree visualization + tests

--- id3.py
+++ id3.py
@@ -1,13 +1,27 @@
 from math import log2
 from numpy import unique
 from pandas import DataFrame, read_csv
+from prettyprint import PrettyPrintTree
+
+
+
+class TreeVisualization():
+    def __init__(self, value):
+        self.children = []
+        self.value = value
+
+    def add_child(self, child):
+        self.children.append(child)
+
+        return child
 
 
 
 class Node():
-    def __init__(self, left=None, right=None, threshold=None, info_gain=None, value=None):
+    def __init__(self, left=None, right=None, feature=None, threshold=None, info_gain=None, value=None):
         self.left = left
         self.right = right
+        self.feature = feature
         self.threshold = threshold
         self.info_gain = info_gain
         self.value = value
@@ -22,9 +36,11 @@ class DecisionTree():
 
 
     def make(self, dataset, target):
+        self.target = target
         self.labels = list(dataset.columns)
         self.labels.remove(target)
-        self.root = self.build_tree(dataset)
+        self.visual_tree = TreeVisualization("root")
+        self.root = self.build_tree(dataset, self.visual_tree)
 
 
     def entropy(self, feature):
@@ -68,6 +84,7 @@ class DecisionTree():
                 if info_gain > max_info_gain:
                     best_split["left"] = left
                     best_split["right"] = right
+                    best_split["feature"] = feature
                     best_split["threshold"] = t
                     best_split["info_gain"] = info_gain
                     max_info_gain = info_gain
@@ -75,7 +92,7 @@ class DecisionTree():
         return best_split
 
 
-    def build_tree(self, dataset, depth=0):
+    def build_tree(self, dataset, visual_tree, depth=0):
         entropies = dict()
 
         if depth <= self.max_depth:
@@ -87,15 +104,39 @@ class DecisionTree():
             best_split = self.get_best_split(dataset, target_feature)
 
             if best_split and best_split["info_gain"] > 0:
-                left_subtree = self.build_tree(best_split["left"], depth+1)
-                right_subtree = self.build_tree(best_split["right"], depth+1)
+                visual_tree = visual_tree.add_child(TreeVisualization(f'{best_split["feature"]} ≤ {best_split["threshold"]}'))
+                left_subtree = self.build_tree(best_split["left"], visual_tree, depth+1)
+                right_subtree = self.build_tree(best_split["right"], visual_tree, depth+1)
 
-                return Node(left_subtree, right_subtree, best_split["threshold"], best_split["info_gain"])
+                return Node(left_subtree, right_subtree, best_split["feature"], best_split["threshold"], best_split["info_gain"])
+
+        remaining = list(dataset[self.target])
+        leaf = max(remaining, key=remaining.count)
+        visual_tree = visual_tree.add_child(TreeVisualization(leaf))
+
+        return Node(value=leaf)
+
+
+    def print_tree(self):
+        pt = PrettyPrintTree(lambda node: node.children, lambda node: node.value)
+        pt(self.visual_tree)
 
 
 
 if __name__ == "__main__":
-    labels = ["sepal_length", "sepal_width", "petal_length", "petal_width", "variety"]
-    data = read_csv("iris.csv", skiprows=1, header=None, names=labels)
-    tree = DecisionTree()
-    tree.make(data, "variety")
+    def iris():
+        labels = ["sepal_length", "sepal_width", "petal_length", "petal_width", "variety"]
+        data = read_csv("iris.csv", skiprows=1, header=None, names=labels)
+        tree = DecisionTree()
+        tree.make(data, "variety")
+        tree.print_tree()
+    
+    def heart():
+        labels = ["age", "anaemia", "creatinine", "diabetes", "ejection", "high bp", "platelets", "serum", "sex", "smoking", "death"]
+        data = read_csv("Heart_Failure_Details.csv", usecols=range(1, len(labels)+1), skiprows=1, header=None, names=labels)
+        tree = DecisionTree()
+        tree.make(data, "death")
+        tree.print_tree()
+    
+    #iris()
+    heart()
